e5ec41f5985bc6f12f87f277305c5bbaadc1870b
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2022-10-13 01:51:11 -0400

    testing 2nd commit

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 30) {
             Text("Hello, world!")
                 .padding()
                 
-            Text("Start Coding Dominic")
-                .padding()
+            HStack() {
+                
+                Image(systemName: "gear")
+                    .padding()
+                    
+                Text("Settings")
+                    .padding()
+                Spacer()
+            }.background(Color.blue)
+              
+            
+            
+                
+            Text.init(.now, format: .dateTime)
+            
+                
         }
+        Spacer()
         
     }
 }
